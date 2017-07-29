f5b67035728b9aaf24a6a7797c32b7919c2d74d6
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-07-28 22:15:59 -0400

    design edits to the HTML

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
 var gameOfLife = {
-  
-  width: 12, 
-  height: 12, // width and height dimensions of the board
+
+  width: 24,
+  height: 24, // width and height dimensions of the board
   stepInterval: null, // should be used to hold reference to an interval that is "playing" the game
 
   createAndShowBoard: function () {
-    
     // create <table> element
     var goltable = document.createElement("tbody");
-    
+
     // build Table HTML
     var tablehtml = '';
     for (var h=0; h<this.height; h++) {
@@ -19,23 +18,17 @@ var gameOfLife = {
       tablehtml += "</tr>";
     }
     goltable.innerHTML = tablehtml;
-    
+
     // add table to the #board element
     var board = document.getElementById('board');
     board.appendChild(goltable);
-    
+
     // once html elements are added to the page, attach events to them
     this.setupBoardEvents();
 
   },
 
   forEachCell: function (iteratorFunc) {
-    /* 
-      Write forEachCell here. You will have to visit
-      each cell on the board, call the "iteratorFunc" function,
-      and pass into func, the cell and the cell's x & y
-      coordinates. For example: iteratorFunc(cell, x, y)
-    */
     for(var i=0; i<gameOfLife.width; i++) {
       for(var j=0; j<gameOfLife.height; j++) {
         var cell00 = document.getElementById(i + '-' + j);
@@ -44,48 +37,31 @@ var gameOfLife = {
     }
 
   },
-  
+
   setupBoardEvents: function() {
-    // each board cell has an CSS id in the format of: "x-y" 
-    // where x is the x-coordinate and y the y-coordinate
-    // use this fact to loop through all the ids and assign
-    // them "click" events that allow a user to click on 
-    // cells to setup the initial state of the game
-    // before clicking "Step" or "Auto-Play"
-    
-    // clicking on a cell should toggle the cell between "alive" & "dead"
-    // for ex: an "alive" cell be colored "blue", a dead cell could stay white
-    
-    // EXAMPLE FOR ONE CELL
-    // Here is how we would catch a click event on just the 0-0 cell
-    // You need to add the click event on EVERY cell on the board
-    
     var onCellClick = function () {
-      // QUESTION TO ASK YOURSELF: What is "this" equal to here?
-      // how to set the style of the cell when it's clicked
       if (this.dataset.status == 'dead') {
         this.className = 'alive';
         this.dataset.status = 'alive';
       } else {
         this.className = 'dead';
         this.dataset.status = 'dead';
-      } 
+      }
     };
-
 
     var onClearClick = function() {
       gameOfLife.forEachCell(function(cell) {
       cell.className = 'dead';
-      cell.dataset.status = 'dead'; 
-    });  
+      cell.dataset.status = 'dead';
+    });
     };
 
-    for(var i=0; i<gameOfLife.width; i++) {
-      for(var j=0; j<gameOfLife.height; j++) {
+    for (var i=0; i<gameOfLife.width; i++) {
+      for (var j=0; j<gameOfLife.height; j++) {
         var cell00 = document.getElementById(i + '-' + j);
         cell00.addEventListener('click', onCellClick);
       }
-    };
+    }
 
     var clear_bt = document.getElementById('clear_btn');
     clear_bt.addEventListener('click', onClearClick);
@@ -93,15 +69,13 @@ var gameOfLife = {
 
     var step_btn = document.getElementById('step_btn');
     step_btn.addEventListener('click', gameOfLife.step);
-    
-  },
 
-  
+  },
 
   randomize: function() {
     forEachCell(function(cell) {
       var rand = Math.round(Math.random());
-      if(rand === 0) {
+      if (rand === 0) {
         cell.className = 'dead';
         cell.dataset.status = 'dead';
       } else {
@@ -111,57 +85,37 @@ var gameOfLife = {
     });
   },
 
-
-
-
   step: function () {
-    // Here is where you want to loop through all the cells
-    // on the board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the next
-    // evolution of the game. 
-    //
-    // You need to:
-    // 1. Count alive neighbors for all cells
-    // 2. Set the next state of all cells based on their alive neighbors
     var queue = [];
-
-    // for(var i=0; i<gameOfLife.width; i++) {
-    //   for(var j=0; j<gameOfLife.height; j++) {
-    //     var cell00 = document.getElementById(i + '-' + j);
-
     gameOfLife.forEachCell(function(cell, i, j) {
       var counter = 0;
-        for(var e=-1; e<2; e++) {
-          for(var l=-1; l<2; l++) {
+        for (var e=-1; e<2; e++) {
+          for (var l=-1; l<2; l++) {
             var neighbor = document.getElementById((i+e) + '-' + (j+l));
-            if(neighbor) {
-              if(neighbor.className === 'alive') {
+            if (neighbor && neighbor !== cell) {
+              if (neighbor.className === 'alive') {
               counter++
               }
-            }  
+            }
           }
         }
 
-        if(counter === 3) {
-              queue.push('alive');
-              // this.dataset.status = 'alive';
-            }
-            else {
-              queue.push('dead');
-              // this.dataset.status = 'dead';
-            }
-        
-            
+        if (counter === 3) {
+          queue.push('alive');
+        }
+        else if (cell.className === 'alive' && counter === 2){
+          queue.push('alive');
+        }
+        else {
+          queue.push('dead');
+        }
     })
-
-
-   
 
     gameOfLife.forEachCell(function(cell) {
        var status = queue.shift();
       console.log(status);
-      
-      if(status === 'alive') {
+
+      if (status === 'alive') {
         cell.className = 'alive';
          cell.dataset.status = 'alive';
       }
@@ -170,29 +124,13 @@ var gameOfLife = {
           cell.dataset.status = 'dead';
         }
     });
-    
-
-
-        // cell00.addEventListener('click', onCellClick);
-
-      // }
-    // }
-
-
-
-
   },
 
   enableAutoPlay: function () {
     // Start Auto-Play by running the 'step' function
-    // automatically repeatedly every fixed time interval  
+    // automatically repeatedly every fixed time interval
   }
-  
+
 };
-
-
-
-
-
 
 gameOfLife.createAndShowBoard();
